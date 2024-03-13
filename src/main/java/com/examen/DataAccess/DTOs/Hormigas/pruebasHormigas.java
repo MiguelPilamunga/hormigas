86dab9a61845fa8d19c8c7c8c824a8cade3d539d
esanta <luis.pilamunga@epn.edu.ec>
2024-03-13 14:53:13 -0500

    1,2,3,4,5

--- src/main/java/com/examen/DataAccess/DTOs/Hormigas/pruebasHormigas.java
+++ src/main/java/com/examen/DataAccess/DTOs/Hormigas/pruebasHormigas.java
@@ -1,16 +1,9 @@
 package com.examen.DataAccess.DTOs.Hormigas;
 
-import com.examen.DataAccess.DTOs.Hormigas.imp.HormigaLarva;
-import com.examen.DataAccess.DTOs.Hormigas.imp.HormigaReina;
-import com.examen.DataAccess.DTOs.Hormigas.imp.HormigaSantacruz;
-import com.examen.DataAccess.DTOs.Hormigas.imp.HormigaSoldado;
 
 public class pruebasHormigas {
     public static void main(String[] args) {
 
-        Hormiga hormigaSantacruz = new HormigaSantacruz();
-
-        hormigaSantacruz.toString();
 
 
     }
